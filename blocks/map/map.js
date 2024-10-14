2505844b27b9c7e10ec50f02553af471cd06895c
Tucker Elliott <telliott@adobe.com>
2024-10-14 15:43:52 -0400

    added new map block

--- blocks/map/map.js
+++ blocks/map/map.js
@@ -1,59 +1,25 @@
-import maps from "https://maps.google.com/maps/api/js?key=AIzaSyDXzFn5v3nI8tvmgI9lDk17bVYszO0ThsI";
+//var long = document.getElementById("49131").innerHTML
+//var lat = document.getElementById("-123091").innerHTML
 
-function initialize() {
-  // Setup map and options
-  const map = new google.maps.Map(document.getElementById("map_canvas"), {
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-  });
+var directions = document.getElementsByClassName("map")[0].innerHTML;
+//var longandlat = directions.match(/(\d+(?:\.\d+)?)/g);
+var longandlat = directions.match(/(-?\d+(?:\.\d+)?)/g);
 
-  let bounds = new google.maps.LatLngBounds();
+var long = longandlat[0];
+var lat = longandlat[1];
 
-  fetch("locations.json")
-    .then((response) => response.json())
-    .then((data) => {
-      // Loop through the data
-      if (Array.isArray(data.data)) {
-        data.data.forEach((item) => {
-          const myLatlng = new google.maps.LatLng(item.lat, item.lng); // set position
+//alert (long + "" + lat)
+//document.getElementById('google-map-location').innerHTML = long + " " + lat;
+const map = document.createElement("p");
+map.id = "google-map-location";
+const location = document.getElementsByClassName("map")[0];
+location.appendChild(map);
 
-          // Add marker to map
-          const marker = new google.maps.Marker({
-            position: myLatlng,
-            map: map,
-          });
-
-          bounds.extend(marker.position); // extend bounds with marker
-
-          // Set info window content
-          let infoContent = "<strong>" + item.name + "</strong>";
-          infoContent += "<p>" + item.address + "</p>";
-
-          // Add info window
-          marker.info = new google.maps.InfoWindow({
-            content: infoContent,
-          });
-
-          // Add listener for info window
-          google.maps.event.addListener(marker, "click", function () {
-            marker.info.open(map, marker);
-          });
-
-          // Add marker location to loc var
-          const loc = new google.maps.LatLng(
-            marker.position.lat(),
-            marker.position.lng()
-          );
-
-          // extend bounds with loc
-          bounds.extend(loc);
-        });
-        map.fitBounds(bounds);
-        map.panToBounds(bounds);
-      } else {
-        console.error("Error: data is not an array");
-      }
-    })
-    .catch((error) => console.error("Error:", error));
-}
-
-export default async function decorate(block) {}
+//document.getElementById('google-map-location').innerHTML = "<object type=\"text/html\" data=\"https://google.com/maps?q=" + long + "," + lat + "\"" + "style=\"width:1400px; height:400px\"></object>";
+document.getElementById("google-map-location").innerHTML =
+  '<object type="text/html" data="https://www.google.com/maps/embed/v1/place?key=AIzaSyDXzFn5v3nI8tvmgI9lDk17bVYszO0ThsI&zoom=12&maptype=roadmap&q=' +
+  long +
+  "," +
+  lat +
+  '"' +
+  'style="width:1200px; height:600px"></object>';
